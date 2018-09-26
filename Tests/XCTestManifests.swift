9507f7310085058668273415798800c72d496a51
rex-remind101 <rex@remind101.com>
2018-09-26 14:15:43 -0700

    [swift-4.2] updated to swift 4.2 and add some linux tests

--- Tests/XCTestManifests.swift
+++ Tests/XCTestManifests.swift
@@ -1,9 +1,35 @@
 import XCTest
 
-#if os(Linux)
-public func allTests() -> [XCTestCaseEntry] {
+extension JSONValueTests {
+    static let __allTests = [
+        ("test0NumberFalseAndNullHashesAreUnique", test0NumberFalseAndNullHashesAreUnique),
+        ("testArrayOfInt8JSONablesProperly", testArrayOfInt8JSONablesProperly),
+        ("testArrayOfIntJSONablesProperly", testArrayOfIntJSONablesProperly),
+        ("testArraySubscripting", testArraySubscripting),
+        ("testArrayToFromJSONConvertsProperly", testArrayToFromJSONConvertsProperly),
+        ("testEarlyNullReturnsNullWhenSubscriptingKeyPath", testEarlyNullReturnsNullWhenSubscriptingKeyPath),
+        ("testFalseAndTrueHashesAreNotEqual", testFalseAndTrueHashesAreNotEqual),
+        ("testHashesAreConsistent", testHashesAreConsistent),
+        ("testJsonBoolCoercesFromNSNumber", testJsonBoolCoercesFromNSNumber),
+        ("testJsonBoolCoercesToNSNumber", testJsonBoolCoercesToNSNumber),
+        ("testJsonNumberCoercesFromNSNumber", testJsonNumberCoercesFromNSNumber),
+        ("testJsonStringCoercesToDouble", testJsonStringCoercesToDouble),
+        ("testJsonStringCoercesToInt", testJsonStringCoercesToInt),
+        ("testJsonStringCoercesToNSNumber", testJsonStringCoercesToNSNumber),
+        ("testJsonStringToNSDate", testJsonStringToNSDate),
+        ("testsEncodeAsString", testsEncodeAsString),
+        ("testSubscriptingReturnsNilIfAttributeIsNonExistent", testSubscriptingReturnsNilIfAttributeIsNonExistent),
+        ("testSubscriptingWithDotsUsesComponentsForTraversal", testSubscriptingWithDotsUsesComponentsForTraversal),
+        ("testSubscriptingWithDotsUsesWholeStringIfComponentsFail", testSubscriptingWithDotsUsesWholeStringIfComponentsFail),
+        ("testUniqueHashesForJSONArrayReordering", testUniqueHashesForJSONArrayReordering),
+        ("testUniqueHashesForKeyValueReorderingOnJSONObject", testUniqueHashesForKeyValueReorderingOnJSONObject),
+    ]
+}
+
+#if !os(macOS)
+public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(JSONValueTests.allTests),
+        testCase(JSONValueTests.__allTests),
     ]
 }
 #endif
